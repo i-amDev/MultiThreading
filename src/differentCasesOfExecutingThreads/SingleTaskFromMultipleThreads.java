1b02251e918d3f91238a466cb605178af003c61d
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-10 15:51:10 +0530

    - Different cases of executing threads :  1. Performing single task from multiple threads.

---
+++ src/differentCasesOfExecutingThreads/SingleTaskFromMultipleThreads.java
@@ -0,0 +1,18 @@
+package differentCasesOfExecutingThreads;
+
+public class SingleTaskFromMultipleThreads extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println("Performing single task from multiple thread.");
+    }
+
+    public static void main(String[] args) {
+
+        SingleTaskFromMultipleThreads thread = new SingleTaskFromMultipleThreads();
+        thread.start();
+
+        SingleTaskFromMultipleThreads newThread = new SingleTaskFromMultipleThreads();
+        newThread.start();
+    }
+}

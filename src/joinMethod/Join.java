ab4ef8393c6368f37e76d6ed25c320c108d1b87f
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-11 18:48:16 +0530

    - Learned about "join" method in Threads. - Used the "join()" method to make the child thread wait for the main thread to complete first.

--- src/joinMethod/Join.java
+++ src/joinMethod/Join.java
@@ -2,10 +2,13 @@ package joinMethod;
 
 public class Join extends Thread{
 
+    static Thread mainThread;
+
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
             try {
+                mainThread.join();
                 Thread.sleep(500);
             }
             catch (InterruptedException e) {
@@ -17,14 +20,10 @@ public class Join extends Thread{
 
     public static void main(String[] args) {
 
+        mainThread = Thread.currentThread();
+
         Join thread = new Join();
         thread.start();
-        try {
-            thread.join();
-        }
-        catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
         for (int i = 1; i <= 5; i++) {
             try {

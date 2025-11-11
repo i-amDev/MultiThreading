0470e7c301500c91be4f69d2e558d950f85b17c1
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-11 19:27:15 +0530

    - Learned about "interrupt()" method in multithreading.

---
+++ src/interruptMethods/Interrupt.java
@@ -0,0 +1,24 @@
+package interruptMethods;
+
+public class Interrupt extends Thread{
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i < 6; i++) {
+                System.out.println(Thread.currentThread().getName() + " -> " + i);
+                Thread.sleep(1000);
+            }
+        }
+        catch (InterruptedException e) {
+            System.out.println("Interrupt method called...." + e);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Interrupt thread = new Interrupt();
+        thread.start();
+        thread.interrupt();
+    }
+}

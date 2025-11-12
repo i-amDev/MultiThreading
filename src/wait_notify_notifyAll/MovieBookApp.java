4914c94b923ff782f0c91ab491b0eef8891f71c1
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-12 13:57:30 +0530

    - Learned about "wait()", "notify()" and "notifyAll()" methods.

---
+++ src/wait_notify_notifyAll/MovieBookApp.java
@@ -0,0 +1,37 @@
+package wait_notify_notifyAll;
+
+class TotalEarnings extends Thread{
+
+    int total = 0;
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                total += 100;
+            }
+            this.notify();
+        }
+    }
+}
+public class MovieBookApp{
+
+    public static void main(String[] args) {
+
+        TotalEarnings earnings = new TotalEarnings();
+        earnings.start();
+
+//        System.out.println(earnings.total);
+
+        synchronized (earnings) {
+            try {
+                earnings.wait();
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Total earning is : " + earnings.total + " Rs");
+        }
+
+    }
+}

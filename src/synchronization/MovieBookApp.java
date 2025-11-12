db43e28ddce20fe76ec05ac7f5eb8fcb1499e830
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-12 11:57:09 +0530

    - Created a "MovieBookApp" class to see what problem was occurring before the use of synchronization.

---
+++ src/synchronization/MovieBookApp.java
@@ -0,0 +1,42 @@
+package synchronization;
+
+class BookSeat {
+    private int totalSeats = 10;
+
+    public void bookSeats(int seats) {
+        if (seats <= totalSeats) {
+            System.out.println("Seat booked successfully");
+            totalSeats -= seats;
+            System.out.println(totalSeats + " seats remaining");
+        }
+        else {
+            System.out.println("Seat can't be booked");
+            System.out.println("Available seats : " + totalSeats);
+        }
+    }
+}
+
+public class MovieBookApp extends Thread{
+
+    static BookSeat bookSeat;
+
+    private int seats;
+
+    @Override
+    public void run() {
+        bookSeat.bookSeats(seats);
+    }
+
+    public static void main(String[] args) {
+
+        bookSeat = new BookSeat();
+
+        MovieBookApp thread1 = new MovieBookApp();
+        thread1.seats = 7;
+        thread1.start();
+
+        MovieBookApp thread2 = new MovieBookApp();
+        thread2.seats = 6;
+        thread2.start();
+    }
+}
